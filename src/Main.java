1adc02b9a00e282fd3d61c1f062ef8aead2e7b59
krishnasharma5547 <krishna.sharma_cs18@gla.ac.in>
2020-04-03 16:15:37 +0530

    binary search program

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         System.out.println("enter size of array: ");
         int size = sc.nextInt();
         int arr[] = new int[size];
         System.out.println("Enter Array Elements: ");
-        for(int i=0;i<size;i++){
+        for (int i = 0; i < size; i++) {
             arr[i] = sc.nextInt();
         }
         System.out.println("Enter element to search: ");
         int ele = sc.nextInt();
+
         LinearSearch ls = new LinearSearch();
-        int result = ls.search(arr,size,ele);
+        int result = ls.search(arr, size, ele);
+        testResult(result);
+
+        BinarySearch bs = new BinarySearch();
+        result = bs.binarySearch(arr, 0, size - 1, ele);
+        testResult(result);
+    }
+
+    static public void testResult(int result) {
         if (result == -1)
             System.out.println("Element not present");
         else
